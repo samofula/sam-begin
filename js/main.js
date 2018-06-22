f30e0314e7c9d5f471511e77baac65cc20775c82
samofula <samofula96@gmail.com>
2018-06-22 14:45:08 +0300

    html Css

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(window).load(function(){
+  $(".topheader").sticky({ topSpacing: 0 });
+});
+
+//Shop tab
+ $(".tab-list li").click(function() {
+        $(".tab-list li").removeClass('active');
+        $(this).addClass('active');
+        
+
+        $('.tab-content').hide().eq($(this).index()).fadeIn(300);
+    });
+$('.tab-list > li').first().click();
+
+
+
+//Shop tab
+ $(".tab-list2 li").click(function() {
+        $(".tab-list2 li").removeClass('active');
+        $(this).addClass('active');
+        $('.tab-content2').hide().eq($(this).index()).fadeIn(300);
+    });
+$('.tab-list2 > li').first().click();
