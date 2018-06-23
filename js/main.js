1f21e2cab66ebbe55d07bb648000525adca7ca17
samofula <samofula96@gmail.com>
2018-06-23 11:51:48 +0300

    all changed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+AOS.init({
+  easing: 'ease-in-out-sine',
+  duration: 1200
+});
+
 $(window).load(function(){
   $(".topheader").sticky({ topSpacing: 0 });
 });
@@ -6,15 +11,11 @@ $(window).load(function(){
  $(".tab-list li").click(function() {
         $(".tab-list li").removeClass('active');
         $(this).addClass('active');
-        
-
         $('.tab-content').hide().eq($(this).index()).fadeIn(300);
     });
 $('.tab-list > li').first().click();
 
-
-
-//Shop tab
+//Shop tab2
  $(".tab-list2 li").click(function() {
         $(".tab-list2 li").removeClass('active');
         $(this).addClass('active');
